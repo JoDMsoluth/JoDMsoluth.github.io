703a96e1f9eaf630574c2e7638a0949eb90b8ddf
jodmsoluth <jodmsoluth@gmail.com>
2020-12-30 23:53:16 +0900

    git flow

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © Copyright {new Date().getFullYear()} By
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://github.com/JoDMsoluth">Jod</a>
       </footer>
     </div>
   )
